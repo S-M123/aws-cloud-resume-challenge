2295e57a20227b681e64c0cd090e5e293d0e1b76
S-M123 <sameeramasood3@gmail.com>
2024-04-22 15:14:13 -0400

    counter

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
     let response = await fetch("https://ezg6ktpmdm3jkcnsbhrl2eggfi0bjafl.lambda-url.us-east-1.on.aws/");
     let data = await response.json();
-    counter.innerHTML = ' views: ${data}';
+    counter.innerHTML =  `views: ${data}`;
 }
 
 updateCounter();
