b9b7c53f640a52c193d592ba03c38343669b665e
S-M123 <sameeramasood3@gmail.com>
2024-04-22 15:52:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const counter = document.querySelector(".counter-number");
 
-// This function updates the counter on my portfiolio
+// This function updates the counter on my portfolio
 async function updateCounter() {
-    let response = await fetch("https://eo6zkbfg3ty6igk4fxe2c5seri0sldln.lambda-url.us-east-1.on.aws/");
+    let response = await fetch("https://ezg6ktpmdm3jkcnsbhrl2eggfi0bjafl.lambda-url.us-east-1.amazonaws.com/");
     let data = await response.json();
-    counter.innerHTML =  `views: ${data}`;
+    counter.innerHTML = `views: ${data.message}`;
 }
 
 updateCounter();
